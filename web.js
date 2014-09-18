7d89fffe34ba0ec2f314fa682f43845dc8a9e830
Francis <goulet.francis@gmail.com>
2014-09-18 09:49:46 -0400

    Manual module load to meet express 4.0 criterias

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
-var gzippo = require('gzippo');
+var gzippo  = require('gzippo');
 var express = require('express');
-var app = express();
+var morgan  = require('morgan'); // express 4.0 need to manually load modules
+var app 	= express();
  
-app.use(express.logger('dev'));
+// app.use(express.logger('dev')); // express 3.0
+app.use(morgan('dev')); // express 4.0
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
