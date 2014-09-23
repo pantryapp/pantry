682493888e7117de5864744f36377adea8dc8a67
Francis <goulet.francis@gmail.com>
2014-09-23 15:30:23 -0400

    Got rid of redundance in api client side

--- app/scripts/api.js
+++ app/scripts/api.js
@@ -1,39 +1,12 @@
 'use strict';
 
 angular.module('pantryApi', ['ngResource'])
-	.factory('API', ['$resource', 'ENV', function($resource, ENV){
-
-	var pantryitems  = $resource(ENV.apiEndpoint + '/pantryitems/:pantryItemId', {pantryItemId: '@id'}, {
-		'update' : {method : 'PUT'}
-	});
-	
+	.service('API', ['$resource', 'ENV', function($resource, ENV){	
 	return{
-		// pantryitems: function(){
-		// 	return{
-		// 		getAll: function(){
-		// 			return pantryitems.query();
-		// 		},
-		// 		get: function(id, success, error){
-		// 			return pantryitems.get({pantryItemId:id}, success);
-		// 		},
-		// 		create: function(item, success, error){
-		// 			pantryitems.save({pantryitem:item}, function(item){
-		// 				success(item);
-		// 			});
-		// 		},
-		// 		update: function(item, success, error){
-		// 			pantryitems.update({pantryItemId:item.id}, {pantryitem:item}, success);
-		// 		},
-		// 		delete: function(id, success, error){
-		// 			pantryitems.remove({pantryItemId:id}, success);
-		// 		}
-		// 	}
-		// },
 		pantryitems: function(){
-			return pantryitems;
-		},
-		pantryitem: function(){
-			return pantryitem;
-		}	
+			return $resource(ENV.apiEndpoint + '/pantryitems/:pantryItemId', {pantryItemId: '@id'}, {
+				'update' : {method : 'PUT'}
+			});
+		}
 	}
 }]);
