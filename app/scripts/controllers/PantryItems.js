5cbd646f29e20180af86da2ebb042658859286fb
Francis Goulet <pantryappgithub@gmail.com>
2015-01-25 17:37:58 -0500

    Make category sortable

--- app/scripts/controllers/PantryItems.js
+++ app/scripts/controllers/PantryItems.js
@@ -16,20 +16,35 @@
 
     vm.create = create;
 
-    vm.items = dataservice.pantryitems().query(function(data) {
-      vm.items = data;
-    });
+    getItems();
+
+    function getItems() {
+      dataservice.pantryitems().query().$promise.then(getItemsComplete, getItemsFailed);
+    }
 
     function create() {
-      dataservice.pantryitems().save({
-        name: vm.newItem.name,
-        category: vm.newItem.category,
-        outofstock: false
 
-      }, function(newItem) {
-        vm.items.push({name: newItem.name, category: newItem.category, outofstock: false});
-        console.log(vm.items);
-      });
+      dataservice.pantryitems().save({
+        name        : vm.newItem.name,
+        category    : vm.newItem.category,
+        outofstock  : false
+      }).$promise.then(createItemComplete, createItemFailed);
+    }
+
+    function getItemsComplete(data) {
+      vm.items = data;
+    }
+
+    function getItemsFailed(error) {
+      console.log('Error while querying items', error);
+    }
+
+    function createItemComplete(newItem) {
+      vm.items.push(newItem);
+    }
+
+    function createItemFailed(error) {
+      console.log('Error while creating an item', error);
     }
 
     function orderBy(key) {
